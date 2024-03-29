9dd9204f9efd61e742ee0826a793ed9678333e36
krishnasoutlookemail <krishnas@outlookemail.ca>
2024-03-28 23:08:21 -0400

    Added files

---
+++ src/practiceupdated/EmployeeDetails.java
@@ -0,0 +1,14 @@
+package practiceupdated;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) {
+
+		int employeeID=10;
+		String employeeName="krishna";
+		System.out.println(employeeID);
+		System.out.println(employeeName);
+
+	}
+
+}
